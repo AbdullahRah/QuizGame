72876587af15149dc4c0fdfe0e88010a882e8a96
Abdullah Rahman <abdullahrahh@gmail.com>
2022-05-18 14:38:46 -0600

    First commit

--- quizgame.py
+++ quizgame.py
@@ -12,3 +12,21 @@ if answer.lower == "central processing unit":
     print("Correct!")
 else:
     print("incorrect!")
+
+answer = input("What does GPU stand for? ")
+if answer.lower == "graphics processing unit":
+    print("Correct!")
+else:
+    print("incorrect!")
+
+answer = input("What does RAM stand for? ")
+if answer.lower == "ranson access memory":
+    print("Correct!")
+else:
+    print("incorrect!")
+
+answer = input("What does PSU stand for? ")
+if answer.lower == "power supply":
+    print("Correct!")
+else:
+    print("incorrect!")
